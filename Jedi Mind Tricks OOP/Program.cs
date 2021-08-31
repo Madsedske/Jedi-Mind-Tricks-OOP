8b56195109c43ee38aab5f81a98c207070152e69
Mads Borregaard Edske Rasmussen <mads7473@zbc.dk>
2021-08-31 12:56:52 +0200

    Add project files.

---
+++ Jedi Mind Tricks OOP/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Jedi_Mind_Tricks_OOP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, int> people = new Dictionary<string, int>();
+            people.Add("Mads", 23);
+            people.Add("Andreas", 35);
+            people.Add("Lars", 64);
+            people["Hans"] = 24;
+
+            people.Remove("Hans");
+
+            foreach (KeyValuePair<string, int> items in people)
+            {
+                Console.WriteLine("{0} {1}", items.Key, items.Value);
+            }
+
+
+            Console.ReadKey();
+        }
+    }
+}
